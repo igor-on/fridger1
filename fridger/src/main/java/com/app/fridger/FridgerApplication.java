276e96e5f516f6aff2f2450e160cf7ca45a35450
Igor N <igor@MacBook-Pro-Igor.local>
2023-04-19 13:42:20 +0200

    initial commit

---
+++ fridger/src/main/java/com/app/fridger/FridgerApplication.java
@@ -0,0 +1,13 @@
+package com.app.fridger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridgerApplication.class, args);
+	}
+
+}
---
+++ fridger/src/test/java/com/app/fridger/FridgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.fridger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
