e6a024aff5f24f6e4840f09d1e0fdb9131b13e96
Igor Nowakowski <igor.nowakowski@hawatel.com>
2023-11-10 14:36:42 +0100

    ADD fridger fe - groceries-list final state

--- fridger-frontend/src/app/app.module.ts
+++ fridger-frontend/src/app/app.module.ts
@@ -1,22 +1,23 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from "./app.component";
-import { RecipesComponent } from "./components/recipes/recipes.component";
-import { SidebarComponent } from "./components/sidebar/sidebar.component";
-import { RecipesListComponent } from "./components/recipes-list/recipes-list.component";
-import { ColorsDirective } from "./common/colors.directive";
-import { AppRoutingModule } from "./app-routing.module";
-import { RecipeDetailsComponent } from "./components/recipe-details/recipe-details.component";
-import { RecipeFormComponent } from "./components/recipe-form/recipe-form.component";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { RecipeCalendarComponent } from "./components/recipe-calendar/recipe-calendar.component";
-import { FullCalendarModule } from "@fullcalendar/angular";
-import { AddEventDialogComponent } from "./components/recipe-calendar/add-event-dialog/add-event-dialog.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatButtonModule } from "@angular/material/button";
+import { AppComponent } from './app.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RecipesListComponent } from './components/recipes-list/recipes-list.component';
+import { ColorsDirective } from './common/colors.directive';
+import { AppRoutingModule } from './app-routing.module';
+import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
+import { RecipeFormComponent } from './components/recipe-form/recipe-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RecipeCalendarComponent } from './components/recipe-calendar/recipe-calendar.component';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { AddEventDialogComponent } from './components/recipe-calendar/add-event-dialog/add-event-dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 import { GroceriesListComponent } from './components/groceries-list/groceries-list.component';
 
 @NgModule({
@@ -42,6 +43,7 @@ import { GroceriesListComponent } from './components/groceries-list/groceries-li
     MatDialogModule,
     FormsModule,
     MatButtonModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
