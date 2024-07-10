95e9b010a8d798033e542ead1c198cebb6564808
Igor Nowakowski <igor.nowakowski@hawatel.com>
2024-07-10 10:28:12 +0200

    MODIFY renamed some files v2

--- fridger-frontend/src/app/app-routing.module.ts
+++ fridger-frontend/src/app/app-routing.module.ts
@@ -4,11 +4,11 @@ import { NgModule } from '@angular/core';
 import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
 import { RecipeFormComponent } from './components/recipe-form/recipe-form.component';
 import { RecipeCalendarComponent } from './components/recipe-calendar/recipe-calendar.component';
-import { recipesResolver } from './recipesResolver';
+import { recipesResolver } from './recipes-resolver';
 import { GroceriesListComponent } from './components/groceries-list/groceries-list.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { authGuard } from './guards/authGuard';
+import { authGuard } from './guards/auth-guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
