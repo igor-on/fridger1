0ffd50507e1bfb0abe097d6a7672b9db6f356a66
Igor Nowakowski <igor.nowakowski@hawatel.com>
2024-10-23 15:39:01 +0200

    ADD fridgeItem real data & navbar fixed

--- fridger-frontend/src/app/components/fridge/fridge-item/fridge-item.component.ts
+++ fridger-frontend/src/app/components/fridge/fridge-item/fridge-item.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { Icon } from 'src/app/shared/icons';
+import { FridgeIngredient } from 'src/app/shared/models/fridge';
 
 @Component({
   selector: 'app-fridge-item',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, DatePipe],
   templateUrl: './fridge-item.component.html',
   styleUrl: './fridge-item.component.scss',
 })
 export class FridgeItemComponent {
   protected readonly Icon = Icon;
+
+  @Input({ required: true }) ingredient!: FridgeIngredient;
 }
