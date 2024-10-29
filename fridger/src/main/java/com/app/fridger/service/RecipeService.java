b78a41c1dadb102c437d004a39c9a2d006864da4
Igor Nowakowski <igor.nowakowski@hawatel.com>
2024-10-29 12:07:49 +0100

    MOVE toEntity method to RecipeMapper

--- fridger/src/main/java/com/app/fridger/service/RecipeService.java
+++ fridger/src/main/java/com/app/fridger/service/RecipeService.java
@@ -2,22 +2,18 @@ package com.app.fridger.service;
 
 import com.app.fridger.client.SpoonacularClient;
 import com.app.fridger.model.api.spoonacular.RecipeInformation;
-import com.app.fridger.model.core.Unit;
 import com.app.fridger.model.dto.RecipeDTO;
-import com.app.fridger.model.entity.Ingredient;
 import com.app.fridger.model.entity.Recipe;
 import com.app.fridger.model.entity.RecipeIngredient;
 import com.app.fridger.repo.RecipeRepository;
-import com.app.fridger.utils.UnitConverter;
+import com.app.fridger.utils.RecipeMapper;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 @Log4j2
@@ -28,7 +24,7 @@ public class RecipeService {
     private final RecipeRepository recipeRepository;
     private final IngredientService ingredientService;
 
-    private final FileService fileService;
+    private final RecipeMapper recipeMapper;
 
     private final SessionService session;
 
@@ -125,7 +121,7 @@ public class RecipeService {
     public List<RecipeDTO> generateRandomRecipes(int number) {
         List<RecipeInformation> randomRecipes = spoonacularClient.getRandomRecipes(number);
 
-        List<Recipe> recipes = randomRecipes.stream().map(this::toEntity).toList();
+        List<Recipe> recipes = randomRecipes.stream().map(recipeMapper::toEntity).toList();
         recipes.forEach(r -> r.setUser(session.getUser()));
 
         recipeRepository.saveAll(recipes);
@@ -133,50 +129,5 @@ public class RecipeService {
         return RecipeDTO.fromEntities(recipes);
     }
 
-    public Recipe toEntity(RecipeInformation recipeInformation) {
-        Recipe recipe = new Recipe();
-        recipe.setName(recipeInformation.getTitle());
-        recipe.setDescription(recipeInformation.getSummary());
-        recipe.setInstructions(recipeInformation.getInstructions());
-        recipe.setImageUrl(recipeInformation.getImage());
-        recipe.setLink(recipeInformation.getSourceUrl());
-        recipe.setFavorite(false);
 
-
-        Map<String, Ingredient> checkedIngredients = new HashMap<>();
-        recipeInformation.getExtendedIngredients().forEach(ei -> {
-            RecipeIngredient recipeIngredient = new RecipeIngredient();
-            if (!checkedIngredients.containsKey(ei.getName())) {
-                checkedIngredients.put(ei.getName(), ingredientService.getOrCreateIngredient(ei.getName(), null));
-            }
-            recipeIngredient.setIngredient(checkedIngredients.get(ei.getName()));
-
-            Unit unit;
-            double quantity;
-            try {
-                log.info("First try - using available units");
-                unit = Unit.fromText(ei.getMeasures().getMetric().getUnitShort());
-                quantity = ei.getMeasures().getMetric().getAmount();
-            } catch (Exception e) {
-                try {
-                    log.info("Second try - using unit converter");
-                    UnitConverter unitConverter = new UnitConverter(ei.getMeasures().getMetric().getUnitShort(), Unit.G);
-                    quantity = unitConverter.convert(ei.getMeasures().getMetric().getAmount());
-                    unit = Unit.G;
-                } catch (Exception ex) {
-                    log.error("Provided unit is currently not parsable... fallback to pieces: " + ei.getMeasures().getMetric().getUnitLong());
-                    fileService.writeFileToResourceFolder("spoonacular-missing-units.txt", ei.getMeasures().getMetric().getUnitLong());
-                    quantity = ei.getMeasures().getMetric().getAmount();
-                    unit = Unit.PCS;
-                }
-            }
-
-            recipeIngredient.setQuantity(quantity);
-            recipeIngredient.setUnit(unit);
-
-            recipe.add(recipeIngredient);
-        });
-
-        return recipe;
-    }
 }
